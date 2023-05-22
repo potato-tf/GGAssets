96b4f829b7a4ccd63047ed32d83a5f9b03c74159
talascio <40523890+talascio@users.noreply.github.com>
2023-05-22 19:19:13 -0400

    lol

--- scripts/kickmorder.lua
+++
@@ -1,9 +0,0 @@
-function OnPlayerConnected(player)
-	player:AddCallback(ON_SPAWN, function(player)
-		if player:IsRealPlayer() then
-			if player.m_szNetworkIDString == "[U:1:103721458]" then
-				player:Kill()
-			end
-		end
-	end)
-end
